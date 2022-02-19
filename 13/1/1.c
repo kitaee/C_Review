64f94ff01a30d5f41d6c14e3e487e6348e26610f
이기태 <91439717+kitaee@users.noreply.github.com>
2022-02-20 01:41:53 +0900

    Update 1.c

--- 13/1/1.c
+++ 13/1/1.c
@@ -4,10 +4,11 @@ int main(void){
     int arr[5]={1,2,3,4,5};
     int *ptr=arr;
     for(int i=0;i<5;i++){
-        *(ptr+i)+=2;
+        *ptr+=2;
+        ptr++;
     }
     for(int i=0;i<5;i++){
-        printf("%d\n",*(ptr+i));
+        printf("%d\n",arr[i]);
     }
     return 0;
 }
